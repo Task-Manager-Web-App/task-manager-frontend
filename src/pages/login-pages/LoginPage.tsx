b5146dc24ed9fc73cb90f873aef7eb2e08b4786d
nuwan98765432 <t3sunu00@students.oamk.fi>
2026-01-26 01:38:19 +0200

    nuwan commit 1

--- src/pages/login-pages/LoginPage.tsx
+++ src/pages/login-pages/LoginPage.tsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
     <div>
-      <h1>Login Page</h1>
+      <h1>Welcome Back</h1>
+      <p>Please enter your credentials to login</p>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+      <p> Don't have an account? Register</p>
     </div>
   );
 }
--- src/pages/login-pages/RegisterPage.tsx
+++ src/pages/login-pages/RegisterPage.tsx
@@ -1,10 +1,51 @@
+import { useState } from "react";
 
 export default function RegisterPage() {
 
-    
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(email, password, role);
+  };
+
   return (
     <div>
-      <h1>Register Page</h1>
+      <h2>Create Account</h2>
+      <p>Join our student developer community</p>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br /><br />
+
+        <select value={role} onChange={(e) => setRole(e.target.value)}>
+          <option value="">Select Role</option>
+          <option value="user">User</option>
+          <option value="admin">Admin</option>
+        </select>
+
+        <br /><br />
+
+        <button type="submit">Create Account</button>
+      </form>
+      <p>Already have an account? Login</p>
     </div>
   );
 }
