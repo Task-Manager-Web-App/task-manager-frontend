dfacfecde58960209e87f042cf18b8fe5337ba4f
RyanWickramaratne <ryandilthusha@gmail.com>
2026-01-27 00:02:12 +0200

    sample commit

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
-      <div className="max-w-6xl mx-auto p-4">
+      <div className="max-w-6xl mx-auto p-0">
         <Routes>
           <Route path="/" element={<TasksPage />} />
           <Route path="/login" element={<LoginPage />} />
