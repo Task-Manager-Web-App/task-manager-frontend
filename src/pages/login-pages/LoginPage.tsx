5b546432846f17b9f373b5e4048d18c21c99219e
nuwan98765432 <t3sunu00@students.oamk.fi>
2026-01-26 14:42:25 +0200

    Added some Tailwind CSS to loging and register pages

--- src/pages/login-pages/LoginPage.tsx
+++ src/pages/login-pages/LoginPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
@@ -10,32 +11,39 @@ export default function LoginPage() {
   };
 
   return (
-    <div>
-      <h1>Welcome Back</h1>
-      <p>Please enter your credentials to login</p>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md bg-white p-8">
+        <h1 className="text-4xl font-bold text-center">Welcome Back</h1>
+        <p className="text-center text-gray-600 mb-6">Please enter your credentials to login</p>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email Address"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full py-3 border border-gray-300 rounded-lg "
+            required
+          />
 
-        <br /><br />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full py-3 border border-gray-300 rounded-lg "
+            required
+          />
 
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <button type="submit" className="w-full py-3 bg-sky-600  text-white py-2 rounded-lg">
+            Login
+          </button>
+        </form>
 
-        <br /><br />
-
-        <button type="submit">Login</button>
-      </form>
-      <p> Don't have an account? Register</p>
+        <p className="text-center text-gray-600">
+          Don't have an account? <Link to="/register" className="text-sky-600 ">Register</Link>
+        </p>
+      </div>
     </div>
   );
 }
--- src/pages/login-pages/RegisterPage.tsx
+++ src/pages/login-pages/RegisterPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function RegisterPage() {
 
@@ -12,40 +13,50 @@ export default function RegisterPage() {
   };
 
   return (
-    <div>
-      <h2>Create Account</h2>
-      <p>Join our student developer community</p>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md bg-white p-8">
+        <h2 className="text-4xl font-bold text-center">Create Account</h2>
+        <p className="text-center text-gray-600 mb-6">Join our student developer community</p>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email Address"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full py-3 border border-gray-300 rounded-lg"
+            required
+          />
 
-        <br /><br />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full py-3 border border-gray-300 rounded-lg"
+            required
+          />
 
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <select
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+            className="w-full py-3 border border-gray-300 rounded-lg"
+            required
+          >
+            <option value="">Select Role</option>
+            <option value="user">User</option>
+            <option value="admin">Admin</option>
+          </select>
 
-        <br /><br />
+          <button type="submit" className="w-full py-3 bg-sky-600  text-white rounded-lg">
+            Create Account
+          </button>
+        </form>
 
-        <select value={role} onChange={(e) => setRole(e.target.value)}>
-          <option value="">Select Role</option>
-          <option value="user">User</option>
-          <option value="admin">Admin</option>
-        </select>
-
-        <br /><br />
-
-        <button type="submit">Create Account</button>
-      </form>
-      <p>Already have an account? Login</p>
+        <p className="text-center text-gray-600 mt-4">
+          Already have an account? <Link to="/login" className="text-sky-600 font-semibold">Login</Link>
+        </p>
+      </div>
     </div>
   );
 }
